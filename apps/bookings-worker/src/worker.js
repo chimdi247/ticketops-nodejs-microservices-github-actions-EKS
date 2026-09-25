690680317c1bb2569380a8ed381fd123bd575afb
Ubuntu <ubuntu@chimdi-vm.lvh2rnzzmvje5jzo02jxi2015f.jnbx.internal.cloudapp.net>
2026-09-25 23:23:08 +0000

    initial commit

--- apps/bookings-worker/src/worker.js
+++ apps/bookings-worker/src/worker.js
@@ -16,8 +16,8 @@ const server = http.createServer(async (req, res) => {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ status: 'ok', service: 'bookings-worker' }));
   } else if (req.url === '/metrics') {
-    res.writeHead(200, { 'Content-Type': client.contentType });
-    res.end(await client.metrics());
+    res.writeHead(200, { 'Content-Type': client.register.contentType });
+    res.end(await client.register.metrics());
   } else {
     res.writeHead(404);
     res.end();
